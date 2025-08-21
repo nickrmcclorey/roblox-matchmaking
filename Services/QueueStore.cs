ba6e882038d32ba39707f1389bcb5a37d7a6bdeb
Nick McClorey <nickrmcclorey@gmail.com>
2025-08-21 12:04:51 -0400

    gamemode must have teamsize in name

--- Services/QueueStore.cs
+++ Services/QueueStore.cs
@@ -1,39 +1,81 @@
 using System.Collections.Concurrent;
 using Matchmaking.Models;
+using Microsoft.AspNetCore.Mvc;
 
-public class QueueStore
-{
+public class QueueStore {
     // TODO: Expose these as IReadOnlyDictionary to prevent external modification
     public ConcurrentDictionary<string, GameMode> Queue = new();
-    public ConcurrentDictionary<int, AutoResetEvent> CancellationTokens = new();
-    public ConcurrentDictionary<int, DatedValue<string>> PlayerResults = new();
-    public const int MAX_PARTY_SIZE = 5;
+    private ConcurrentDictionary<int, AutoResetEvent> CancellationTokens = new();
+    private ConcurrentDictionary<int, DatedValue<string>> PlayerResults = new();
+    private const int MAX_PARTY_SIZE = 6;
 
-    public void AddToQueue(string gameMode, string region, int leaderId, int partySize) {
-        if (!Queue.ContainsKey(gameMode)) {
-            Queue[gameMode] = new GameMode(partySize);
-        }
-
-        if (!Queue[gameMode].ContainsKey(region)) {
-            Queue[gameMode][region] = new List<ConcurrentQueue<int>>(5)
-            {
-                new ConcurrentQueue<int>(),
-                new ConcurrentQueue<int>(),
-                new ConcurrentQueue<int>(),
-                new ConcurrentQueue<int>(),
-                new ConcurrentQueue<int>()
-            };
-        }
-
+    public WaitResult AddToQueue(string gameModeKey, string regionKey, int leaderId, int partySize) {
         if (partySize > MAX_PARTY_SIZE) {
-            throw new BadHttpRequestException($"Party size cannot exceed {MAX_PARTY_SIZE}");
+            return WaitResult.BadRequest($"Party size cannot exceed {MAX_PARTY_SIZE}");
         }
 
         if (CancellationTokens.ContainsKey(leaderId)) {
-            throw new BadHttpRequestException($"Leader ID {leaderId} already in queue");
+            return WaitResult.BadRequest($"Leader ID {leaderId} already in queue");
+        }
+
+        if (!Queue.TryGetValue(gameModeKey, out var gameMode)) {
+            if (!gameModeKey.Contains('-') || !Int32.TryParse(gameModeKey.Split('-')[1], out int teamSize)) {
+                return WaitResult.BadRequest("Game mode must be in format <name>-<team size>");
+            }
+
+            Queue[gameModeKey] = new GameMode(teamSize);
+            gameMode = Queue[gameModeKey];
+        }
+
+        if (!gameMode.TryGetValue(regionKey, out var regionQueue)) {
+            gameMode[regionKey] = new List<ConcurrentQueue<int>>(gameMode.TeamSize);
+            regionQueue = gameMode[regionKey];
+            for (int i = 0; i < gameMode.TeamSize; i++) {
+                regionQueue.Add(new ConcurrentQueue<int>());
+            }
         }
 
         CancellationTokens[leaderId] = new AutoResetEvent(false);
-        Queue[gameMode][region][partySize - 1].Enqueue(leaderId);
+        regionQueue[partySize - 1].Enqueue(leaderId);
+        return WaitForQueueResult(leaderId);
     }
+
+    public WaitResult WaitForQueueResult(int playerId) {
+
+        // When the matchmaker creates a game, it puts the result in PlayerResults BEFORE removing the CancellationToken
+        // It's important to check the Cancellation token before checking PlayerResults to avoid a race condition
+        if (!CancellationTokens.TryGetValue(playerId, out var wait)) {
+            if (!PlayerResults.TryGetValue(playerId, out var accessCode)) {
+                return WaitResult.BadRequest($"Player {playerId} not found in queue");
+            }
+            return WaitResult.Ready(accessCode.Value);
+        }
+
+        wait.WaitOne(1000 * 30);
+        if (!PlayerResults.ContainsKey(playerId)) {
+            return WaitResult.StillWaiting();
+        }
+
+        if (!PlayerResults.TryRemove(playerId, out var code)) {
+            return WaitResult.Error("Match created but could not remove access code from dictionary");
+        }
+
+        return WaitResult.Ready(code.Value);
+    }
+
+    public void CreateMatch(List<int> players, string accessCode) {
+        foreach (var player in players) {
+            PlayerResults[player] = new DatedValue<string>(accessCode);
+            CancellationTokens[player].Set();
+            CancellationTokens.TryRemove(player, out _);
+        }
+    }
+
+    public void FailedToQueuePlayers(List<int> players) {
+        foreach (var player in players) {
+            CancellationTokens[player].Set();
+            CancellationTokens.TryRemove(player, out _);
+        }
+    }
+
 }
